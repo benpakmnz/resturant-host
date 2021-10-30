f01968100a6e2e1c854aa45a9d28170366746a4b
benny <benpakmnz@gmail.com>
2021-10-31 01:22:50 +0200

    styling app

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,198 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import './utilitis/normalize.scss';
-import Table from './components/Table/table';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Table from "./components/Table/Table";
+import moment from "moment";
+import {
+  Alert,
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  CssBaseline,
+  Drawer,
+  List,
+  ListItemAvatar,
+  ListItemText,
+  ListSubheader,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import ListItemButton from "@mui/material/ListItemButton";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import Divider from "@mui/material/Divider";
+import { AlertColor } from "@mui/material/Alert/Alert.d";
 
 export interface tableInterface {
-  Concat: number[],
-  Diners: number,
-  Table: number,
-  Order?: orderInterface 
+  Concat: number[];
+  Diners: number;
+  Table: number;
+  Order?: orderInterface;
 }
 
 export interface orderInterface {
-  Diners: number,
-  Mobile: string,
-  start_time?: Date,
-  end_time?: Date,
-  Table?: number[]
+  Diners: number;
+  Mobile: string;
+  start_time?: string;
+  end_time?: string;
+  Table?: number[];
 }
 
-export interface completedOrder {
-
-}
+const drawerWidth = 350;
 
 const App: React.FC = () => {
-  const [tableList, setTableList]= useState<tableInterface[]>([]);
+  const [tableList, setTableList] = useState<tableInterface[]>([]);
   const [ordersList, setOrdersList] = useState<orderInterface[]>([]);
   const [completedOrders, setCompletedOrders] = useState<orderInterface[]>([]);
+  const [alertType, setAlertType] = useState<AlertColor | undefined>("error");
+  const [alertText, setAlertText] = useState<string>("");
 
-useEffect(() => {
-  setTableList(require('./utilitis/floor.json'));
-  setOrdersList(require('./utilitis/orders.json'));
-}, []);
+  useEffect(() => {
+    setTableList(require("./utilitis/floor.json"));
+    setOrdersList(require("./utilitis/orders.json"));
+  }, []);
 
-const ServeWaitingList = () => {
-  let tempTableList = [...tableList];
-  let tempOrderList = [...ordersList];
-  tempTableList.forEach((table: tableInterface) => {
-    tempOrderList.forEach((order: orderInterface, index: number) =>{
-      if(order.Diners === table.Diners && !table.Order){
-        table.Order = order;
-        table.Order.start_time = new Date();
-        tempOrderList.splice(index, 1);
+  const serveWaitingList = () => {
+    if (ordersList.length > 0) {
+      //check for free table
+      if (tableList.find((table) => !table.Order)) {
+        let tempTableList = [...tableList];
+        let tempOrderList = [...ordersList];
+
+        // find fit table
+        tempTableList.forEach((table: tableInterface) => {
+          tempOrderList.forEach((order: orderInterface, index: number) => {
+            if (order.Diners === table.Diners && !table.Order) {
+              // setting the order into the table and setting start time stamp
+              table.Order = order;
+              table.Order.start_time = moment(new Date()).format(
+                "DD/MM/YYYY, HH:mm"
+              );
+
+              // removing order from orderlist
+              tempOrderList.splice(index, 1);
+            }
+          });
+        });
+        setTableList(tempTableList);
+        setOrdersList(tempOrderList);
+      } else {
+        setAlertType("error");
+        setAlertText("there are no avivalble tables for serving");
       }
-    })
-  })
-  setTableList(tempTableList)
-  setOrdersList(tempOrderList)
+    } else {
+      setAlertType("warning");
+      setAlertText("There are no waiting orders");
+    }
+  };
 
-}
+  const writeServedOrders = (tableId: number) => {
+    let tempTableList = [...tableList];
 
-const clearTable = (tableId: number) => {
-  let tempTableList = [...tableList];
-  let tableIndex = tempTableList.findIndex(table => table.Table === tableId);
-  tempTableList[tableIndex].Order!.end_time = new Date();
-  tempTableList[tableIndex].Order!.Table = [tempTableList[tableIndex].Table]
-  setCompletedOrders([...completedOrders, tempTableList[tableIndex].Order!])
-  delete tempTableList[tableIndex].Order;
-  console.log(completedOrders)
-}
+    // finding table index in the table list
+    let tableIndex = tempTableList.findIndex(
+      (table) => table.Table === tableId
+    );
 
+    // setting end time stamp
+    tempTableList[tableIndex].Order!.end_time = moment(new Date()).format(
+      "DD/MM/YYYY, HH:mm"
+    );
+
+    // setting up table id into the completed order
+    tempTableList[tableIndex].Order!.Table = [tempTableList[tableIndex].Table];
+    setCompletedOrders([...completedOrders, tempTableList[tableIndex].Order!]);
+
+    // removing order from table item
+    delete tempTableList[tableIndex].Order;
+    console.log(completedOrders);
+  };
 
   return (
-    <div>
-    {tableList.length > 0 && (
-      <ul className="tables_list">
-        {tableList.map((tableItem: tableInterface) => (
-          <Table tableData = {tableItem}  clearTable ={clearTable} key = {tableItem.Table}/>
-        ))}
-      </ul>
-    )}
-      <button onClick={ServeWaitingList}>setTables</button>
-    {ordersList.length > 0 && (
-            <ul>
-            {ordersList.map((orderItem: orderInterface, index) => (
-              <li key = {index}></li>
+    <>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Resturant Host Planner
+            </Typography>
+            <Button
+              variant="outlined"
+              color="inherit"
+              startIcon={<RestaurantIcon />}
+              onClick={serveWaitingList}
+            >
+              Serve waiting list
+            </Button>
+          </Toolbar>
+          {alertText !== "" && (
+            <Stack sx={{ width: "100%" }} spacing={2}>
+              <Alert onClose={() => setAlertText("")} severity={alertType}>
+                {alertText}
+              </Alert>
+            </Stack>
+          )}
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            {ordersList.length > 0 ? (
+              <List
+                subheader={
+                  <ListSubheader>Orders wating for service</ListSubheader>
+                }
+              >
+                {ordersList.map((orderItem: orderInterface, index) => (
+                  <React.Fragment key={index}>
+                    <ListItemButton>
+                      <ListItemAvatar>
+                        <Avatar />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={`Amount of guests: ${orderItem.Diners}`}
+                        secondary={`Contact number: ${orderItem.Mobile}`}
+                      />
+                    </ListItemButton>
+                    <Divider variant="inset" />
+                  </React.Fragment>
+                ))}
+              </List>
+            ) : (
+              <Typography align="center" sx={{ mt: 20 }}>
+                There are no waiting orders
+              </Typography>
+            )}
+          </Box>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <ul className="tables_list">
+            {tableList.map((tableItem: tableInterface) => (
+              <Table
+                tableData={tableItem}
+                clearTable={writeServedOrders}
+                key={tableItem.Table}
+              />
             ))}
           </ul>
-    )}  
-    </div>
+        </Box>
+      </Box>
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Table/table.tsx
+++ src/components/Table/table.tsx
@@ -1,65 +1,138 @@
-import React, { useEffect, useState } from 'react';
-import {tableInterface} from '../../App';
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Modal,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import PeopleIcon from "@mui/icons-material/People";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import { tableInterface } from "../../App";
 import "./table.scss";
 
+const Table: React.FC<{
+  tableData: tableInterface;
+  clearTable: (tableId: number) => void;
+}> = (props) => {
+  const [seconds, setSeconds] = useState(0);
+  const [isShowInfo, setShowInfo] = useState<boolean>(false);
 
-const Table:React.FC<{tableData:tableInterface, clearTable: (tableId: number) => void}> = (props) => {
-    const [seconds, setSeconds ] = useState(0);
-
-    useEffect(()=> {
-      if(seconds > 10){
-        props.clearTable(props.tableData.Table)
-      }
-        let interval:any = null;
-        if(props.tableData.Order){
-            interval = setInterval(() => {
-                setSeconds(seconds => seconds + 1);
-              }, 1000);
-        } else if (!props.tableData.Order && seconds !== 0) {
-            clearInterval(interval);
-            setSeconds(0)
-        }
-        return () => clearInterval(interval);   
-    }, [props, seconds]);
-
-    const tableType = () => {
-      if(props.tableData.Diners === 1){
-        return "circle";
-      }else if(props.tableData.Diners === 2){
-        return "wide_circle";
-      }else if(props.tableData.Diners === 3){
-        return "triangle-up";
-      }else if(props.tableData.Diners === 4){
-        return "square";
-      }else if(props.tableData.Diners >= 5){
-        return "pentagon";
-      }
+  useEffect(() => {
+    if (seconds > 90) {
+      props.clearTable(props.tableData.Table);
     }
-    
+    let interval: any = null;
 
+    if (props.tableData.Order) {
+      //setting interval to check the time passed
+      // only for table that gets an order
 
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!props.tableData.Order && seconds !== 0) {
+      clearInterval(interval);
+      setSeconds(0);
+    }
+    return () => clearInterval(interval);
+  }, [props, seconds]);
 
-    return (
-        <li className="table_container">
-          <button className="table_btn" disabled={!props.tableData.Order}>
-            <span className="table_id">{props.tableData.Table}</span>
-            <span className={`table_shape ${tableType()} ${seconds === 0? " clear" : seconds < 10? " accupied" : " almost-done"}`} ></span>
-          </button>
-          {/* <p>Id: {props.tableData.Table},  
-          Capacity: {props.tableData.Diners},
-          Avaliable : {props.tableData.Order? 'false': 'true'},
-          <span>time passed: {seconds}</span>  
-          </p>
-        {props.tableData.Order && (
-            <div>
-               amount of pepole: {props.tableData.Order.Diners}, 
-               phone number: {props.tableData.Order.Mobile},
-            </div>
-        )} */}
-        </li>
+  // table shape based on diners amount
+  const tableType = () => {
+    if (props.tableData.Diners === 1) {
+      return "circle";
+    } else if (props.tableData.Diners === 2) {
+      return "wide_circle";
+    } else if (props.tableData.Diners === 3) {
+      return "triangle-up";
+    } else if (props.tableData.Diners === 4) {
+      return "square";
+    } else if (props.tableData.Diners >= 5) {
+      return "pentagon";
+    }
+  };
 
+  return (
+    <React.Fragment>
+      <li className="table_container">
+        <button
+          className="table_btn"
+          disabled={!props.tableData.Order}
+          onClick={() => setShowInfo(true)}
+        >
+          <span
+            className={`table_shape ${tableType()} ${
+              // handeling color of table based on interval value
+              seconds === 0
+                ? " clear"
+                : seconds < 60
+                ? " accupied"
+                : " almost-done"
+            }`}
+          ></span>
+          {props.tableData.Order && <RestaurantIcon className="tabled_order" />}
+        </button>
+        <Typography align="center" variant="caption">
+          Table number: {props.tableData.Table}
+        </Typography>
+      </li>
 
-    )
-}
+      <Modal open={isShowInfo} onClose={() => setShowInfo(false)}>
+        <div className="info_modal">
+          <Typography variant="h6" align="center">
+            Table number: {props.tableData.Table}, now Serves:
+          </Typography>
+          {props.tableData.Order ? (
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <PhoneAndroidIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Order number"
+                  secondary={props.tableData.Order?.Mobile}
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <PeopleIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Amount of pepole:"
+                  secondary={props.tableData?.Order!.Diners}
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <AccessTimeFilledIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Started at"
+                  secondary={props.tableData.Order?.start_time}
+                />
+              </ListItem>
+            </List>
+          ) : (
+            // handle case when table gets cleared while info modal is open
+            <Typography variant="body2" align="center">
+              Nobody, its avaliable to get new orders
+            </Typography>
+          )}
+        </div>
+      </Modal>
+    </React.Fragment>
+  );
+};
 
 export default Table;
